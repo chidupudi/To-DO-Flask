2c0fbbcfef7bd43a49e3e74c7c298c90366ddf43
Rupesh Chidupudi <chrupesh2425@gmail.com>
2024-08-14 11:47:15 +0530

    changes

--- app.py
+++ app.py
@@ -1,44 +1,38 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    done = db.Column(db.Boolean, default=False)
+# MongoDB configuration
+app.config["MONGO_URI"] = "mongodb://localhost:27017/todo_db"
+mongo = PyMongo(app)
+
+# Get the tasks collection
+tasks_collection = mongo.db.tasks
 
 @app.route('/')
 def index():
-    tasks = Task.query.all()
+    tasks = tasks_collection.find()
     return render_template('index.html', tasks=tasks)
 
 @app.route('/add', methods=['POST'])
 def add_task():
     title = request.form.get('title')
     if title:
-        new_task = Task(title=title)
-        db.session.add(new_task)
-        db.session.commit()
+        tasks_collection.insert_one({"title": title, "done": False})
     return redirect(url_for('index'))
 
-@app.route('/toggle/<int:task_id>')
+@app.route('/toggle/<task_id>')
 def toggle_task(task_id):
-    task = Task.query.get_or_404(task_id)
-    task.done = not task.done
-    db.session.commit()
+    task = tasks_collection.find_one({"_id": ObjectId(task_id)})
+    tasks_collection.update_one({"_id": ObjectId(task_id)}, {"$set": {"done": not task['done']}})
     return redirect(url_for('index'))
 
-@app.route('/delete/<int:task_id>')
+@app.route('/delete/<task_id>')
 def delete_task(task_id):
-    task = Task.query.get_or_404(task_id)
-    db.session.delete(task)
-    db.session.commit()
+    tasks_collection.delete_one({"_id": ObjectId(task_id)})
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
